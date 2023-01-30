b463e1b25b4e5143087736d5da2d85bac8254740
Sam Larkin <selarkin96@gmail.com>
2023-01-30 17:43:45 +0000

    Refactoring and adding docstrings

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # noughtsandcrosses
-Simple implementation of noughts and crosses (tic tac toe)
+
+A simple, pure Python3 implementation of noughts and crosses (tic tac toe),
+playable at the command line (with two players using a single computer).
+
+## Example terminal output
+
+```
+     A   B   C
+   +---+---+---+
+0  |   |   |   |
+   +---+---+---+
+1  |   |   |   |
+   +---+---+---+
+2  |   |   |   |
+   +---+---+---+
+
+X moves first (randomly selected)
+
+Make your move, player X!
+Type the coordinates (e.g. A0) of the square in
+which you would like to place a marker:
+...B1
+
+     A   B   C
+   +---+---+---+
+0  |   |   |   |
+   +---+---+---+
+1  |   | X |   |
+   +---+---+---+
+2  |   |   |   |
+   +---+---+---+
+
+Make your move, player O!
+Type the coordinates (e.g. A0) of the square in
+which you would like to place a marker:
+...
+
+```
--- noughtsandcrosses.py
+++ noughtsandcrosses.py
@@ -1,9 +1,8 @@
 """noughtsandcrosses
 
-A simple implementation of noughts and crosses (tic tac toe), playable in
-a terminal.
+A simple implementation of noughts and crosses (tic tac toe), playable at the
+command line (with two players using a single computer).
 
-Sam Larkin
 """
 from random import choice
 
@@ -12,60 +11,85 @@ class Board:
     """Noughts and crosses game board"""
 
     def __init__(self):
+        """Set up the game board for noughts and crosses
+
+        Initialises Board.state as a 2D list representing the 3x3 game board,
+        displays the board to the players and randomly selects the player (O or
+        X) to make the first move.
+
+        """
         row = [" "] * 3
         self.state = [row[:] for _ in range(3)]
-        self.first_move = choice("OX")
-        self.show()
-        print(f"{self.first_move} Move first (randomly selected)")
-        self.mover = self.first_move
+        self.mover = choice("OX")
+        print(self)
+        print(f"\n{self.mover} moves first (randomly selected)")
 
-    def show(self):
-        """Show the game board"""
+    def __repr__(self):
+        """Displays the game board"""
         col_names = "     A   B   C  "
         hline = "   +---+---+---+"
-        print(col_names)
-        print(hline)
+        repr_lines = [col_names, hline]
+
         for index, row in enumerate(self.state):
-            print(f"{index}  | {row[0]} | {row[1]} | {row[2]} |")
-            print(hline)
-        print()
+            repr_lines.append(f"{index}  | {row[0]} | {row[1]} | {row[2]} |")
+            repr_lines.append(hline)
+
+        return "\n".join(repr_lines)
 
     def end_turn(self):
-        if self.mover == "O":
-            self.mover = "X"
-        elif self.mover == "X":
-            self.mover = "O"
-        else:
-            raise ValueError("mover should be O or X")
-        self.show()
+        """End the current player's turn
 
-    def move(self, coord):
-        try:
-            assert isinstance(coord, str)
-            assert len(coord) == 2
-        except AssertionError:
-            print("Coordinate must be a string of the form: A0")
-            player_input = input(f"Make your move, player {self.mover}!\n")
-            self.move(player_input)
-            return
+        Changes Board.mover at end of current player's (i.e. Board.mover's)
+        turn and displays the board to the players.
 
+        """
         try:
-            x_map = {"a": 0, "b": 1, "c": 2}
-            x = x_map[coord[0].lower()]
+            i = "OX".index(self.mover) - 1
+            self.mover = "OX"[i]
+
+        except ValueError:
+            print(f"Something went wrong! Current player should be O or X, " \
+                  f"not {self.mover}")
+            
+        print(self)
+
+    def move(self):
+        """Allows player to make a move
+
+        Receives player input, validates it and places that player's marker.
+        Loops until valid player input is received.
+
+        """
+        x_map = {"a": 0, "b": 1, "c": 2}
+
+        while True:
+            coord = input(
+                f"\nMake your move, player {self.mover}!\n" \
+                f"Type the coordinates (e.g. A0) of the square in \n" \
+                f"which you would like to place a marker:\n..."
+            )
+            print()
+            
+            x_key = coord[0].lower()
             y = int(coord[1])
-            assert self.state[y][x] == " "
-            self.state[y][x] = self.mover
-        except IndexError:
-            print("Coordinate must be a string of the form: A0")
-            player_input = input(f"Make your move, player {self.mover}!\n")
-            self.move(player_input)
-            return
-        except AssertionError:
-            print("Invalid move. Pick an empty square.")
-            player_input = input(f"Make your move, player {self.mover}!\n")
-            self.move(player_input)
-            return
-        self.end_turn()
+
+            try:
+                assert len(coord) == 2
+                assert x_key in ["a", "b", "c"]
+                assert y in [0, 1, 2]
+                x = x_map[x_key]
+                assert self.state[y][x] == " "
+                break
+
+            except AssertionError:
+                print(
+                    "Invalid input. Coordinate must be of the form A0, B1 \n" \
+                    "etc. (case insensitive), and must correspond to an \n" \
+                    "empty square on the game board."
+                )
+                continue
+
+        self.state[y][x] = self.mover
 
     def _check_rows(self):
         for row in self.state:
@@ -102,37 +126,48 @@ class Board:
             return True
         return False
 
-    def stalemate(self):
+    def game_over(self):
+        """Returns True if the game is over else returns False"""
         if self.game_won() is True:
-            return False
+            return True
+
         for row in self.state:
             if " " in row:
                 return False
-        print("Stalemate. No more valid moves are possible.")
-        return True
 
-    def game_over(self):
-        if self.game_won() is True:
-            return True
-        elif self.stalemate() is True:
-            return True
-        return False
+        return True
 
 
 def _three_in_a_row(seq):
-    if seq[0] == seq[1] and seq[1] == seq[2] and seq[0] != " ":
+    """Checks if a list meets the three in a row winning condition
+
+    Args:
+        seq(list): Sequence of values to check for three in a row
+
+    Returns:
+            bool: True if winning condition is met else false
+
+    """
+    if seq[0] == seq[1] and seq[0] == seq[2] and "OX".find(seq[0]) != -1:
         return True
+
     return False
 
 
 def main():
+    """Main game logic"""
     board = Board()
-    while board.game_over() is False:
-        player_input = input(f"Make your move, player {board.mover}!\n")
-        board.move(player_input)
-    if board.stalemate() is False:
+
+    while not board.game_over():
+        board.move()
+        board.end_turn()
+    
+    if board.game_won():
         winner = "OX".replace(board.mover, "")
         print(f"Congratulations, {winner}! You have won!")
+
+    else:
+        print("Draw")
 
 
 if __name__ == "__main__":
